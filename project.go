c5ce034a3137239ca630b18ea818e9286be1d1de
jiankunking <jiankunking@163.com>
2019-12-02 09:54:44 +0800

    修改Project.deleted类型为interface

--- project.go
+++ project.go
@@ -14,7 +14,7 @@ type Project struct {
 	Name         string            `json:"name"`
 	CreationTime time.Time         `json:"creation_time"`
 	UpdateTime   time.Time         `json:"update_time"`
-	Deleted      bool              `json:"deleted"`
+	Deleted      interface{}       `json:"deleted"`
 	OwnerName    string            `json:"owner_name"`
 	Togglable    bool              `json:"togglable"`
 	Role         int               `json:"current_user_role_id"`
